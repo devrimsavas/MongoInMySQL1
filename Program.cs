540a948856261fae408cd4f6530003bb267b3e99
Devrim Savas <137702797+devrimsavas@users.noreply.github.com>
2024-09-28 02:31:10 +0200

    added new

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Dapper;
 using MySqlConnector;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using System.Text.Json.Serialization;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -66,6 +69,92 @@ app.MapGet("/getallbooks", async (MySqlConnection connection) =>
         return Results.Problem("failed", ex.Message);
     }
 });
+
+//extract with id 
+app.MapGet("/getjsondetail/{id}", async (MySqlConnection connection, int id) =>
+{
+    try
+    {
+        var aBook = await connection.QueryAsync<Book>($"SELECT JSON_EXTRACT(attributes,'$.*') As Attributes FROM Books WHERE id=@id", new { id });
+        return Results.Ok(aBook);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("failed", ex.Message);
+    }
+
+});
+
+app.MapGet("/getonepet/{id}", async (MySqlConnection connection, int id) =>
+{
+    try
+    {
+        var pet = await connection.QueryAsync<Pet>($"SELECT * FROM pets WHERE id=@id", new { id });
+        return Results.Ok(pet);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Failes", ex.Message);
+    }
+
+
+}
+
+
+);
+
+//update json part of an id 
+app.MapPut("/updatepet/{id}", async (MySqlConnection connection, int id) =>
+{
+    try
+    {
+        var query = @"UPDATE pets 
+                    SET attributes = JSON_SET(attributes, '$.color', 'red and blue')
+                    WHERE id = @id";
+
+
+        var rowsAffected = await connection.ExecuteAsync(query, new { id });
+        return Results.Ok($"Updated {rowsAffected} row(s).");
+
+
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Failes", ex.Message);
+    }
+});
+
+//add new data to an existing JSON column 
+
+app.MapPut("/newfeature/{id}", async (MySqlConnection connection, int id) =>
+{
+
+    try
+    {
+
+        var doctor = "Joe Vet";
+        var lastVaccine = "rabbies 2024";
+
+        var query = @"UPDATE pets 
+                    SET attributes=JSON_SET(attributes, '$.doctor',@doctor, '$.lastVaccine', @vaccine) WHERE id=@id";
+
+        var parameters = new { doctor, vaccine = lastVaccine, id };
+        var rowsAffected = await connection.ExecuteAsync(query, parameters);
+        return Results.Ok($"Updated {rowsAffected} rows");
+
+
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("faile", ex.Message);
+    }
+
+
+
+});
+
+
+
 
 
 
